87d3e1f1ab2cfac83b75806574fb4e434fd66a91
Karthiksp19 <138290917+Karthiksp19@users.noreply.github.com>
2026-02-01 20:37:00 +0530

    Time: 0 ms (100%), Space: 23.4 MB (96.19%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int ele1 = nums[0];
-        int ele2 = INT_MIN;
-        int count1 = 1;
-        int count2 = 0;
-
         int n = nums.size();
-
-        for (int i = 1; i < n; i++){
-            if (ele1 == nums[i]){
-                count1++;
-            }
-            else if(count1 == 0 && nums[i] != ele2){
+        int count1 = 0, ele1 = -1;
+        int count2 = 0, ele2 = -1;
+        vector<int> ans;
+        for (int i = 0; i < n; i++){
+            if(count1 == 0 && ele2 != nums[i]){
                 ele1 = nums[i];
                 count1 = 1;
             }
-            else if(ele2 == nums[i]){
-                count2++;
-            }
-            else if(count2 == 0 && nums[i] != ele1){
+            else if(count2 == 0 && ele1 != nums[i]){
                 ele2 = nums[i];
                 count2 = 1;
+            }
+            else if(nums[i] == ele1){
+                count1++;
+            }
+            else if(nums[i] == ele2){
+                count2++;
             }
             else{
                 count1--;
                 count2--;
+                if(count1 < 0) count1 = 0;
+                if(count2 < 0) count2 = 0;
             }
         }
 
-        count1 = 0;
-        count2 = 0;
-        for (int x : nums){
-            if (ele1 == x) count1++;
-            else if(ele2 == x) count2++;
+        count1 = 0, count2 = 0;
+        for (int i = 0; i < n; i++){
+            if(ele1 == nums[i]) count1++;
+            else if(ele2 == nums[i]) count2++;
         }
 
-        vector<int> ans;
-
-        cout << count1;
-        if (count1 > n / 3) ans.push_back(ele1);
-        if (count2 > n / 3) ans.push_back(ele2);
-
+        if(count1 > (n / 3)) ans.push_back(ele1);
+        if(count2 > (n / 3)) ans.push_back(ele2);
         return ans;
     }
 };
